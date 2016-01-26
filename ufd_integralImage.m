c09649a661ad045a9bc05125db419629e2d5752d
Aldebaro <aldebaro@ufpa.br>
2016-01-26 09:07:32 -0300

    Outputs the integral image of a gray-scale image.

---
+++ ufd_integralImage.m
@@ -0,0 +1,6 @@
+function I=ufd_integralImage(X)
+% function I=ufd_integralImage(X)
+%Input: X is a matrix representing a gray-scale image
+%Output: I is the integral image of X
+
+I=X; %TO DO: actual calculation
