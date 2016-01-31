2c8b1fa3c4cbe47a2025c6e87c6d40dcfc79ce0e
Marcos Ferreira <fspmarcos@gmail.com>
2016-01-31 05:30:36 -0300

    Added actual calculation for the integral image in the function 'ufd_integralImage' using cumulative sums

--- ufd_integralImage.m
+++ ufd_integralImage.m
@@ -1,6 +1,14 @@
 function I=ufd_integralImage(X)
-% function I=ufd_integralImage(X)
-%Input: X is a matrix representing a gray-scale image
-%Output: I is the integral image of X
+    % function I=ufd_integralImage(X)
+    % Input: X is a matrix representing a gray-scale image
+    % Output: I is the integral image of X
 
-I=X; %TO DO: actual calculation
+    % upcasting the matrix representing the image to double
+    % to make sure we don't lose data 
+    X = im2double(X);
+    % first the cumulative sum of the columns than another cumulative
+    % sum nested for the rows with the new values to calculate the 
+    % integral image (or the summed area table)
+    % TODO do this calculation using for loops
+    I = cumsum(cumsum(X, 2), 1);
+end
