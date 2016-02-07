16c0a7eed77630b9e8901cc121578731d1810b67
Aldebaro Klautau <aldebaro@users.noreply.github.com>
2016-02-07 17:22:36 -0300

    Example of main code, by UFPA

--- ViolaJones_version0b/ufpa_run.m
+++ ViolaJones_version0b/ufpa_run.m
@@ -12,8 +12,11 @@
 %one used in the latest OpenCV version.
 
 %% 1) Define file names
+%XML with new format, obtained from the latest OpenCV:
 %fileName = 'C:/ak/Works/2016_opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml';
+%XML with old format, from fileexchange:
 fileName = 'C:\ak\Works\2016_opencv\ViolaJones_version0b\HaarCascades\haarcascade_frontalface_alt.xml';
+
 %imageFileName = 'C:/ak/Works/2016_opencv/build3/bin/Debug/photo.jpg';
 imageFileName = 'C:\ak\Works\2016_opencv\ViolaJones_version0b\Images\1.jpg';
 %imageFileName = 'C:\ak\Works\2016_opencv\ViolaJones_version0b\Images\2.jpg';
