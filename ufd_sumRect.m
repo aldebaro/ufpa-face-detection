3e01e81038352c0b5d2dc4e5aba167dbb61b4885
Bruno Ricardo <brunoricardojava@gmail.com>
2016-02-18 20:33:59 -0300

    ufd_sumRect.m add

--- ufd_sumRect.m
+++ ufd_sumRect.m
@@ -1,6 +1,15 @@
-function s=ufd_sumRect(integralImage, x, y, w, h)
+function PixelSum=GetSumRect(IntegralImage,x,y,Width,Height)
 % This function returns the sum of pixels inside a rectangle, using the integral
 % image structure to simplify calculations.
 % Inputs: integral image representation and a rectangle defined by the upperleft
 % point (x,y) and its width w and height h
-% (Based on code by D. Kroon)
+% (Based on code by D. Kroon)%
+% PixelSum=GetSumRect(IntegralImage,x,y,Width,Height)
+%
+
+%Size of IntegralImage in frist 
+IIWidth=size(IntegralImage,1);
+PixelSum  =   IntegralImage((x+Width)*IIWidth + y + Height+1) +  ...
+			  IntegralImage(x*IIWidth+y+1) - ...
+			  IntegralImage((x+Width)*IIWidth+y+1) - ...
+			  IntegralImage(x*IIWidth+y+Height+1);
