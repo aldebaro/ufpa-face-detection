583d13cd8a00c526393bf6efde0f3f364b890539
Bruno Ricardo <brunoricardojava@gmail.com>
2016-02-18 20:52:34 -0300

    Add script ufd_sumRect.m

--- ufd_sumRect.m
+++ ufd_sumRect.m
@@ -7,8 +7,10 @@ function PixelSum=GetSumRect(IntegralImage,x,y,Width,Height)
 % PixelSum=GetSumRect(IntegralImage,x,y,Width,Height)
 %
 
-%Size of IntegralImage in frist 
+%Size of IntegralImage in frist dimension
 IIWidth=size(IntegralImage,1);
+
+%Sum of pixels
 PixelSum  =   IntegralImage((x+Width)*IIWidth + y + Height+1) +  ...
 			  IntegralImage(x*IIWidth+y+1) - ...
 			  IntegralImage((x+Width)*IIWidth+y+1) - ...
