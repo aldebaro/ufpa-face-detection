00f95d9027b1ba34501c1a15ba4fbe27dd9d5fa1
Marcos Ferreira <merkkp@gmail.com>
2016-02-19 01:06:30 -0300

    working ufd_test ufd_test now theoretically runs based only on the ufd_* functions (tested only in octave) Some bugfixes, copied HaarCascades folder from ViolaJones_version0b to base folder, added a new image, and that's it

--- ufd_test.m
+++ ufd_test.m
@@ -6,30 +6,44 @@
 imageFileName = ['Images', filesep(), '2.jpg'];
 
 %And now add the path for the Haar features
+fileName = ['HaarCascades', filesep(), 'haarcascade_frontalface_alt.xml'];
 
 %in Dr. Kroon's code the xml was removed, but since undestanding
 %its conversion to .MAT is one of the points of the project, I'm going to include it
-fileName = ['HaarCascades', filesep(), 'haarcascade_frontalface_alt.xml'];
 %name treatment for the .xml file
 j=find(fileName=='.'); 
 if(~isempty(j))
     j=j(end);
     fileName=fileName(1:j-1); 
 end
-%Creates a string with the name of the .xml, but with .mat extension
-fileNameHaarCascade = [fileName '.mat'];
+
 %check if converted file already exixts, if so, conversion won't be done
 if (~exist([fileName, '.mat'])) 
     ufd_convertXML(fileName)
 end
+
+%gets the name of the .mat created above from the opencb xml file
+fileNameHaarCascade = [fileName '.mat'];
+
+%read it
+haarCascade = ufd_readHaar(fileNameHaarCascade);
+
 %stores the image in a matrix called 'I'
-I = imread(imageFileName);
+img = imread(imageFileName);
+
+% gets its integral image
+intImg = ufd_integralImage(img);
+
 %calls the function that will call other detection functions
 %passes as parameter the image and the .mat features file
-objects = ufd_multiScaleDetection(I, fileNameHaarCascade)
+% and some options
+defaultoptions=struct('ScaleUpdate',1/1.2,'Resize',false,'Verbose',true);
+objects = ufd_multiScaleDetection(intImg, haarCascade, defaultoptions);
+
 %now the part missing is to print in the image the results of the detection
 %draws boxes around the detected objects
-ufd_showBoundingBoxes(I,objects);
+ufd_showBoundingBoxes(img,objects);
+
 %IMPORTANT
 %notice that as this is written, the ufd_showBoundingBoxes has yet nothing in it, 
 %it might be necessary to change the parameters after the function if finished
