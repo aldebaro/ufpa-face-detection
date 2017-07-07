f9c5892e60d8bd638daa58e1b8285d961c310ee0
Aldebaro <aldebaro@ufpa.br>
2017-07-07 12:32:34 -0300

    Improved explanation.

--- ufd_treeDetect.m
+++ ufd_treeDetect.m
@@ -1,7 +1,8 @@
-function Tree_sum=ufd_treeDetect(Node,Tree,Scale,x,y,integralImage,stddev,inverseArea)
+function weakLearnerOutput=ufd_treeDetect(Node,Tree,Scale,x,y,integralImage,stddev,inverseArea)
 % This function performs object detection for one tree classifier.
 % (Based on code by D. Kroon)
-%AK: Note that a "tree" classifier as the one below:
+%AK: Note that the default "tree" classifier trained by OpenCV
+%is organized in the XML file as the one below:
 %          <!-- tree 0 -->
 %           <_>
 %             <!-- root node -->
@@ -14,7 +15,7 @@ function Tree_sum=ufd_treeDetect(Node,Tree,Scale,x,y,integralImage,stddev,invers
 %             <left_val>0.0337941907346249</left_val>
 %             <right_val>0.8378106951713562</right_val></_></_>
 %         <_>
-% is organized as a vector of 21 numbers:
+% which is then stored as a vector of 21 numbers in this code:
 % Tree =
 %   Columns 1 through 6
 %    threshold left_val  right_val    ??        ??      rectangle
@@ -52,31 +53,47 @@ Rectangle_sum = Rectangle_sum * inverseArea; %normalize by area in order
 %to better compare rectangles with distinct sizes / areas
 
 % Get the values of the current haar-classifiers
-LeafTreshold=Leaf(:,1);
-LeftValue = Leaf(:,2);
+LeafTreshold=Leaf(:,1); %this is the threshold
+%find the weak-learner output
+%output in case the the response is smaller than the threshould 
+%multiplied by the standard deviation stddev:
+LeftValue = Leaf(:,2); 
+%output otherwise (response larger or equal the threshould * stddev
 RightValue =Leaf(:,3);
-LeftNode = Leaf(:,4);
-RightNode =Leaf(:,5);
-%titled=Leaf(:,21);
 
-% Check the haar-response versus the haar-classifier treshold
+% Check the haar-response for the specific candidate window
+% against the previously obtained (during training stage) haar-classifier treshold
 % If the haar-response is larger, use the right node/value otherwise the
 % left node/value
 Node=zeros(size(x));
-Tree_sum =zeros(size(x));
+weakLearnerOutput =zeros(size(x));
+%check will be 1 if the right value should be used or 0 otherwise
 check=Rectangle_sum >= LeafTreshold(:).*stddev;
-Node(check) =RightNode(check);
-Node(~check)=LeftNode(~check);
-Tree_sum(check)=RightValue(check);
-Tree_sum(~check)=LeftValue(~check);
+weakLearnerOutput(check)=RightValue(check);
+weakLearnerOutput(~check)=LeftValue(~check);
 
-% If a Node has a larger value then -1, it is not the end node with
-% a value, but it is connected to another weak-classifier
-check=Node>-1;
-if(any(check))
-    %recursively go over the weak classifiers. This allows a more
-    %flexible weak classifier, but here we are using simple
-    %decision stumps, and the code below will not execute
-    warning('AK: Be aware that a more sophisticated classifier is being used');
-    Tree_sum(check)=ufd_treeDetect(Node(check),Tree,Scale,x(check),y(check),integralImage,stddev(check),inverseArea);
+%the logic regarding nodes below gives support to trees as weak learners, but the
+%default detector uses single stumps, so their value is -1 for this
+%detector. AK: I am then run the lines below only if a leaf is different
+%from -1
+if Leaf(1,4)~=-1 ||  Leaf(1,5)~=-1
+    warning('AK: Be aware that a tree classifier more sophisticated than a decision stump is being used');
+    
+    LeftNode = Leaf(:,4);
+    RightNode =Leaf(:,5);
+    %titled=Leaf(:,21);
+    
+    Node(check) =RightNode(check);
+    Node(~check)=LeftNode(~check);
+    
+    % If a Node has a larger value then -1, it is not the end node with
+    % a value, but it is connected to another weak-classifier
+    check=Node>-1;
+    if(any(check))
+        %recursively go over the weak classifiers. This allows a more
+        %flexible weak classifier, but here we are using simple
+        %decision stumps, and the code below will not execute for the
+        %default detector
+        weakLearnerOutput(check)=ufd_treeDetect(Node(check),Tree,Scale,x(check),y(check),integralImage,stddev(check),inverseArea);
+    end
 end
